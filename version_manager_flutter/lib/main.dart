63abf90a808576a77b69f3dac20c24e59f5def7e
Maksim <maks-vasilev-2017@inbox.ru>
2026-02-13 01:14:21 +0500

    save

--- version_manager_flutter/lib/main.dart
+++ version_manager_flutter/lib/main.dart
@@ -41,7 +41,7 @@ String _getServerUrl() {
 
   if (kIsWeb) {
     // Для web используем текущий домен
-    return '${Uri.base.scheme}://${Uri.base.host}/api';
+    return '${Uri.base.scheme}://${Uri.base.host}';
   } else {
     if (urlFromEnv.isNotEmpty) {
       return urlFromEnv;
